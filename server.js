d83cbe6dc18f8a3ce5c08f5d398251f76c56bca7
sinyusuk <7504shlee@naver.com>
2021-07-07 18:48:15 +0900

    set_model_and_mgs.js

--- server.js
+++ server.js
@@ -4,4 +4,23 @@ bodyParser = require('body-parser');
 var {mongoose} = require('./database/mgs');
 var {Todo} = require('./database/model_todos');
 var {User} = require('./database/model_users');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+    var todo = new Todo({
+        text:req.body.text
+    });
+    todo.save().then((doc)=>{
+        res.send(doc);
+    }, (e)=>{
+        res.send('error',e);
+    });
+})
+
+app.listen(3000,()=>{
+    console.log('ready!')
+})
 //https://m.blog.naver.com/ijoos/221202873770
